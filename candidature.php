1772b0d53f67b32f88c61836f56c4a62a4956db5
salma <salmalechkar07@gmail.com>
2026-04-14 00:04:19 +0100

    feat:affichage conditionnel des erreurs

--- candidature.php
+++ candidature.php
@@ -54,7 +54,16 @@ endif;
     <title>Document</title>
 </head>
 <body>
-        
+        <?php if (!empty($erreurs)&& $_SERVER['REQUEST_METHOD']==="POST"):
+         ?>
+
+        <ul class="erreurs">
+            <?php foreach($erreurs as $e):?>
+                <li><?php echo $e;?></li>
+            <?php endforeach;?>
+        </ul>
+
+        <?php endif; ?>
         <div class="container">
             <form action="candidature.php" method="POST">
 
